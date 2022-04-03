cc0a4b22057ba7a01de8d846fe7f24dad033fff6
Tarlan Huseynov <tarlan.hosseinov@gmail.com>
2022-04-04 01:26:49 +0400

    streamlined

--- myhttp/main.go
+++ myhttp/main.go
@@ -3,11 +3,19 @@ package myhttp
 import (
 	"io"
 	"net/http"
+	"text/template"
 )
 
+var Tpl template.Template
+
 func Index(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "text/html; encoding=utf8")
-	io.WriteString(w, "<h1> Hello from index </h1>")
+	//w.Header().Set("Content-type", "text/html; encoding=utf8")
+	Tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+func Me(w http.ResponseWriter, r *http.Request) {
+	//w.Header().Set("Content-type", "text/html; encoding=utf8")
+	r.FormValue("fname")
+	Tpl.ExecuteTemplate(w, "me.gohtml", r.Form["fname"][0])
 }
 
 func Dog(w http.ResponseWriter, r *http.Request) {
@@ -15,7 +23,6 @@ func Dog(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "<h1> Bark from a dog </h1>")
 }
 
-func Me(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "text/html; encoding=utf8")
-	io.WriteString(w, "<h1> Tarlan Huseynov </h1>")
+func init() {
+	template.Must(Tpl.ParseGlob("myhttp/templates/*"))
 }
